6cbd8785735d95047a16c8785664b397e51f2f63
SonnyLuong <sonnyluong9@yahoo.com>
2019-10-16 06:32:40 -0700

    Update Triangle.java
    Pg. 589

--- Ch 13: Recursion/Triangle.java
+++ Ch 13: Recursion/Triangle.java
@@ -1,1 +1,26 @@
-/** 
+/**
+	
+*/
+public class Triangle
+{
+	private int width;
+	/**
+		Constructs a triangular shape
+		@param aWidth the width and height of the triangle
+	*/
+	public Triangle(int awidth)
+	{
+		width = awidth;
+	}
+	public int getArea()
+	{
+		if (width <= 0) { return 0;}
+		else if (width == 1) {return 1;}
+		else 
+		{
+			Triangle smallerTriangle = new Triangle(width - 1);
+			int smallerArea = smallerTriangle.getArea();
+			return smallerArea + width;
+		}
+	}
+}
